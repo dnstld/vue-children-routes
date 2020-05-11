b03ee885a82e250da614c434e00a1af407027ba7
Denis Toledo <contato@denistoledo.com.br>
2020-05-11 10:56:58 -0300

    create nested route with children option

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,6 +28,8 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <router-view />
   </div>
 </template>
 
